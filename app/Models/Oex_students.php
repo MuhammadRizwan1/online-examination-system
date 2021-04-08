b4636eaedaade4ed4a78d86ae49b302046b978d5
rizwan <rizwan.devp@gmail.com>
2021-04-08 19:23:54 +0500

    all model created

---
+++ app/Models/Oex_students.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Oex_students extends Model
+{
+    use HasFactory;
+    protected $table = 'oex_students';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name','email','mobile_no','category','exam','password','status'];
+}
